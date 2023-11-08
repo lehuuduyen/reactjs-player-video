a84f544c856c2f6e4580a38dc38500d3cfbe5b16
admin <lehuuduyen96@gmail.com>
2023-11-09 00:50:10 +0700

    i

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import {
+  Player,
+  ControlBar,
+  ReplayControl,
+  ForwardControl,
+  CurrentTimeDisplay,
+  TimeDivider,
+  PlaybackRateMenuButton,
+  VolumeMenuButton
+} from 'video-react';
+import { Button, Input, Select, Space } from 'antd';
+
+export default props => {
+  const [linkTemp, setLnkTemp] = useState("https://v3.szjal.cn/20191101/PZzNpqB1/index.m3u8");
+  const [linkVideo, setLinkVideo] = useState("https://v3.szjal.cn/20191101/PZzNpqB1/index.m3u8");
+
+  function onchange(event){
+    setLnkTemp(event.target.value)
+  }
+  function onclick(){
+    setLinkVideo(linkTemp)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <Player poster="/assets/poster.png">
+      <source src="https://v3.szjal.cn/20191101/PZzNpqB1/index.m3u8" fill="true"
+    fluid="true"
+    autoplay="true"
+    controls="true" preload='metadata' type='application/x-mpegURL'/>
 
-export default App;
+      <ControlBar>
+        <ReplayControl seconds={10} order={1.1} />
+        <ForwardControl seconds={30} order={1.2} />
+        <CurrentTimeDisplay order={4.1} />
+        <TimeDivider order={4.2} />
+        <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
+        <VolumeMenuButton disabled />
+      </ControlBar>
+    </Player>
+    <Space.Compact style={{ width: '90%' }}>
+      <Input onChange={onchange} defaultValue={true} />
+      <Button type="primary" onClick={onclick}>Play</Button>
+    </Space.Compact>
+    </>
+  );
+};
