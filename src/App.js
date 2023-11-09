591c17370036de059eabde8b24cc0f24d03687c5
ICDVN-DUYENLH\User <lehuuduyen>
2023-11-09 10:42:08 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,49 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import {
-  Player,
-  ControlBar,
-  ReplayControl,
-  ForwardControl,
-  CurrentTimeDisplay,
-  TimeDivider,
-  PlaybackRateMenuButton,
-  VolumeMenuButton
-} from 'video-react';
-import { Button, Input, Select, Space } from 'antd';
+import { Button, Input, Select, Space } from "antd";
+import ReactPlayer from "react-player";
 
-export default props => {
-  const [linkTemp, setLnkTemp] = useState("https://v3.szjal.cn/20191101/PZzNpqB1/index.m3u8");
-  const [linkVideo, setLinkVideo] = useState("https://v3.szjal.cn/20191101/PZzNpqB1/index.m3u8");
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  const [linkTemp, setLnkTemp] = useState();
+  const [linkVideo, setLinkVideo] = useState(
+    "https://v3.szjal.cn/20191101/PZzNpqB1/index.m3u8"
+  );
 
-  function onchange(event){
-    setLnkTemp(event.target.value)
+  function onchange(event) {
+    setLnkTemp(event.target.value);
   }
-  function onclick(){
-    setLinkVideo(linkTemp)
+  function onclick() {
+    setLinkVideo(linkTemp);
   }
-  
+
   return (
     <>
-    <Player poster="/assets/poster.png">
-      <source src="https://v3.szjal.cn/20191101/PZzNpqB1/index.m3u8" fill="true"
-    fluid="true"
-    autoplay="true"
-    controls="true" preload='metadata' type='application/x-mpegURL'/>
-
-      <ControlBar>
-        <ReplayControl seconds={10} order={1.1} />
-        <ForwardControl seconds={30} order={1.2} />
-        <CurrentTimeDisplay order={4.1} />
-        <TimeDivider order={4.2} />
-        <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
-        <VolumeMenuButton disabled />
-      </ControlBar>
-    </Player>
-    <Space.Compact style={{ width: '90%' }}>
-      <Input onChange={onchange} defaultValue={true} />
-      <Button type="primary" onClick={onclick}>Play</Button>
-    </Space.Compact>
+      <div style={{ width:"100%",height:"500px" }}>
+        <ReactPlayer controls={true} width="100%"  style={{background:"black",cursor:"pointer"}}  height="100%" url={linkVideo} />
+        <Space.Compact className="input">
+          <div  style={{ width: "90%" }}><Input onChange={(e) => onchange(e)} defaultValue={linkVideo} /></div>
+          <Button type="primary" onClick={onclick}>
+            Play
+          </Button>
+        </Space.Compact>
+      </div>
     </>
   );
 };
